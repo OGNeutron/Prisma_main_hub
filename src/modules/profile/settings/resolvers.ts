5830e73995d93ba3008c0f225434bf40cabe2ab0
Scott Daniel <scottberry91@gmail.com>
2019-01-01 19:08:08 +0000

    latest changes

--- src/modules/profile/settings/resolvers.ts
+++ src/modules/profile/settings/resolvers.ts
@@ -2,9 +2,51 @@ import { Context } from '../../../tstypes'
 import { logger } from '../../../utils/logger'
 import { ApolloError } from 'apollo-server'
 import { MutationResolvers } from '../../../generated/graphqlgen'
+import { processUpload } from '../../../utils/helperFunctions'
 
 export const resolvers = {
 	Mutation: {
+		async updateProfile(
+			_: any,
+			{ avatar }: any,
+			{ db, session, s3 }: Context
+		) {
+			try {
+				const response: any = await processUpload(s3, avatar, 'avatars')
+
+				if (!response) {
+					return new ApolloError('Error with uploading image')
+				}
+
+				const userImage = await db
+					.user({ id: session.userId })
+					.avatar_url()
+
+				await s3.deleteObject({
+					Bucket: process.env.AWS_BUCKET as string,
+					Key: userImage.key
+				})
+
+				const avatarResponse: any = await db.updateFile({
+					where: {
+						id: userImage.id
+					},
+					data: {
+						mimetype: response.mimetype,
+						filename: response.filename,
+						url: response.url,
+						encoding: response.encoding,
+						key: response.key,
+						ETag: response.Etag
+					}
+				})
+
+				return avatarResponse
+			} catch (error) {
+				logger.error({ level: '5', message: error.message })
+				throw new ApolloError(error)
+			}
+		},
 		blockUser(
 			_: any,
 			{ id }: MutationResolvers.ArgsBlockUser,
--- src/utils/helperFunctions.ts
+++ src/utils/helperFunctions.ts
@@ -1,4 +1,7 @@
-import { Response } from 'express';
+import { Response } from 'express'
+import { v4 as uuid } from 'uuid'
+import { S3 } from 'aws-sdk'
+import { stringType } from 'aws-sdk/clients/iam'
 
 export const respond = (
 	res: Response,
@@ -9,5 +12,43 @@ export const respond = (
 	return res.status(status).json({
 		success: boolean,
 		result: data
-	});
-};
+	})
+}
+
+interface IProcessUpload {
+	filename: stringType
+	mimetype: stringType
+	encoding: stringType
+	key: string
+	url: string
+	Etag: string
+}
+
+export const processUpload = async (
+	s3: S3,
+	picture: string,
+	folder: string
+): Promise<IProcessUpload> => {
+	const { createReadStream, filename, mimetype, encoding }: any = await picture
+    const key = folder + '/' + uuid() + '-' + filename
+    
+    const stream = createReadStream()
+
+	const response = await s3
+		.upload({
+			Bucket: process.env.AWS_BUCKET || 'mainhubbucket',
+			Key: key,
+			ACL: 'public-read',
+			Body: stream
+		})
+		.promise()
+
+	return {
+		filename,
+		mimetype,
+		encoding,
+		key: response.Key,
+		url: response.Location,
+		Etag: response.ETag
+	}
+}
